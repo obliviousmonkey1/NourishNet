856296a0246937c7a7a421da77c9365e0bc6c3f4
Thomas Elliott <70280680+obliviousmonkey1@users.noreply.github.com>
2024-01-30 19:43:59 +0000

    Json implemented

--- src/main/java/com/nourishnet/User.java
+++ src/main/java/com/nourishnet/User.java
@@ -2,36 +2,43 @@ package com.nourishnet;
 
 import java.util.ArrayList;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 public class User {
-    private int ID;
     private String username;
     private int age;
     private float height;
-    private FitnessLevel fitnessLevel;
+    //private FitnessLevel fitnessLevel;
     private int weight;
     private int diet;
     private ArrayList<Integer> savedRecipeIDs;
     private String password;
     
     
-    private static int nextID = 0;
     
     // 28/01/23 : TE : This is for when creating a user  
-    public User(){
-        this("user", "");
-        //password = Tools.encrypt(password, secretKey);
+    // public User(){
+    //     this("", -1, 0.0, -1, -1, new ArrayList<Integer>(), ""); // Note: Use null for password, and ArrayList<> instead of new ArrayList<Integer>()
+    //     //password = Tools.encrypt(password, secretKey);
 
-        // need to call the createUserJson() in the SerializeUserData to save the new users details
-    }
+    //     // need to call the createUserJson() in the SerializeUserData to save the new users details
+    //}
 
     // 28/01/23 : TE : This is for loading existing users 
-    public User(String username, String password){
+    @JsonCreator
+    public User(@JsonProperty("username") String username, @JsonProperty("age") int age, 
+                @JsonProperty("height") float height, @JsonProperty("weight") int weight, 
+                @JsonProperty("diet") int diet, @JsonProperty("savedRecipeIDs") ArrayList<Integer> savedRecipeIDs, 
+                @JsonProperty("password") String password) {
         this.username = username;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.diet = diet;
+        this.savedRecipeIDs = savedRecipeIDs;
         this.password = password;
-        this.ID = nextID;
-
-        nextID++;
     }
  
     public void setPassword(String password){
