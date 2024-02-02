df1f565f73bb610e4aa46fbfdf6e094819db7e7f
Thomas Elliott <70280680+obliviousmonkey1@users.noreply.github.com>
2024-02-02 17:02:29 +0000

    Saving user data

--- src/main/java/com/nourishnet/LogIn.java
+++ src/main/java/com/nourishnet/LogIn.java
@@ -9,37 +9,36 @@ import javax.swing.ImageIcon;
 
 
 public class LogIn {
-	static String userDir = System.getProperty("user.dir");
 	static String extension = "json";
 
-    // 25/01/23 : TE : Gets the names and profile photos of users
+    // 25/01/24 : TE : Gets the names and profile photos of users
     public static List<StringImagePair> getUserProfiles(){
-        File userProfileDir = new File(userDir + Pointers.usersPath);
+        File userProfileDir = new File(Pointers.userDir + Pointers.usersPath);
         File[] listOfFiles = userProfileDir.listFiles();
         List<StringImagePair> profileList = new ArrayList<>();
-        String imgPath;
+        String imagePath;
 
         for(int i=0; i < listOfFiles.length; i++){
             if(listOfFiles[i].isDirectory()){
                 String username = listOfFiles[i].getName();
                 System.out.println(listOfFiles[i].getPath());
 
-                StringBooleanPair hImage =  hasImage(username, listOfFiles[i].getPath());
-                if(hImage.getHasImage()){
-                    imgPath = listOfFiles[i].getPath() +"/"+ username + hImage.getExtension();
+                StringBooleanPair imageData =  hasImage(username, listOfFiles[i].getPath());
+                if(imageData.getHasImage()){
+                    imagePath = listOfFiles[i].getPath() +"/"+ username + imageData.getExtension();
                 }else{
-                    imgPath = userDir + Pointers.usersPath + "/default.png";
+                    imagePath = Pointers.userDir + Pointers.usersPath + "/default.png";
                 }
-                profileList.add(new StringImagePair(username, new ImageIcon(imgPath)));
+                profileList.add(new StringImagePair(username, new ImageIcon(imagePath)));
                 
             }
         }
         return profileList;
     }
 
-    // 25/01/23 : TE : Gets the number of user profiles if 0 then asks user to create one
+    // 25/01/24 : TE : Gets the number of user profiles if 0 then asks user to create one
     public static int getNumberOfUserProfiles(){
-        File userProfileDir = new File(userDir + Pointers.usersPath);
+        File userProfileDir = new File(Pointers.userDir + Pointers.usersPath);
         File[] listOfFiles = userProfileDir.listFiles();
         
         int numberOfProfiles = 0;
@@ -54,16 +53,17 @@ public class LogIn {
 
     }
 
+    // 02/02/24 : TE : Gets the selectedUsersJsonPath
     public static String getUserJsonPath(String username){
-        return userDir + Pointers.usersPath + "/" + username + "/" + username + ".json";
+        return Pointers.userDir + Pointers.usersPath + "/" + username + "/" + username + ".json";
     }
 
-    // 25/01/23 : TE : Checks if file name ends with specified extension
+    // 25/01/24 : TE : Checks if file name ends with specified extension
     private static boolean checkFileExtension(String fileName, String extension){
         return fileName.toLowerCase().endsWith("." + extension.toLowerCase()); 
     }
 
-    // 25/01/23 : TE : Checks if an image exists with that name 
+    // 25/01/24 : TE : Checks if an image exists with that name 
     public static StringBooleanPair hasImage(String imageName, String path) {
         String[] fileExtensions = {".jpg", ".jpeg", ".png", ".gif"};
         for(String extension: fileExtensions){
@@ -77,7 +77,7 @@ public class LogIn {
 	}
 }
 
-// 25/01/23 : TE : Love you Josh <3
+// 25/01/24 : TE : Love you Josh <3
 class StringImagePair {
     private String text;
     private ImageIcon image;
--- src/main/java/com/nourishnet/User.java
+++ src/main/java/com/nourishnet/User.java
@@ -3,21 +3,18 @@ package com.nourishnet;
 import java.util.ArrayList;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 
 public class User {
     private String username;
     private int age;
     private float height;
-    //private FitnessLevel fitnessLevel;
     private int weight;
     private int diet;
     private ArrayList<Integer> savedRecipeIDs;
     private String password;
-    
-    
-    
+
     // 28/01/23 : TE : This is for when creating a user  
     // public User(){
     //     this("", -1, 0.0, -1, -1, new ArrayList<Integer>(), ""); // Note: Use null for password, and ArrayList<> instead of new ArrayList<Integer>()
@@ -27,10 +24,11 @@ public class User {
     //}
 
     // 28/01/23 : TE : This is for loading existing users 
+
     @JsonCreator
-    public User(@JsonProperty("username") String username, @JsonProperty("age") int age, 
-                @JsonProperty("height") float height, @JsonProperty("weight") int weight, 
-                @JsonProperty("diet") int diet, @JsonProperty("savedRecipeIDs") ArrayList<Integer> savedRecipeIDs, 
+    public User(@JsonProperty("username") String username, @JsonProperty("age") int age,
+                @JsonProperty("height") float height, @JsonProperty("weight") int weight,
+                @JsonProperty("diet") int diet, @JsonProperty("savedRecipeIDs") ArrayList<Integer> savedRecipeIDs,
                 @JsonProperty("password") String password) {
         this.username = username;
         this.age = age;
@@ -40,40 +38,71 @@ public class User {
         this.savedRecipeIDs = savedRecipeIDs;
         this.password = password;
     }
- 
-    public void setPassword(String password){
-        this.password = password;
-    }
 
-    public String getUsername(){
+
+    public String getUsername() {
         return this.username;
-    };
-
-
-    public Boolean getHasPassword(){
-        return !getPassword().equals("");
     }
 
-    public float getHeight(){
-        return this.height;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    private String getPassword(){
-        return this.password;
+    public int getAge() {
+        return this.age;
     }
 
-    public Boolean checkPassword(String enteredPassword){
-
-        return getPassword().equals(enteredPassword);
-        
-    }
-
-    public ArrayList<Integer> getSavedRecipeIDs(){
-        return this.savedRecipeIDs;
-    }
-
-    public void changeAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
+    public float getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getDiet() {
+        return this.diet;
+    }
+
+    public void setDiet(int diet) {
+        this.diet = diet;
+    }
+
+    public ArrayList<Integer> getSavedRecipeIDs() {
+        return this.savedRecipeIDs;
+    }
+
+    public void setSavedRecipeIDs(ArrayList<Integer> savedRecipeIDs) {
+        this.savedRecipeIDs = savedRecipeIDs;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonIgnore
+    public Boolean getHasPassword() {
+        return !getPassword().equals("");
+    }
+
+    @JsonIgnore
+    public Boolean checkPassword(String enteredPassword) {
+        return getPassword().equals(enteredPassword);
+    }
 }
