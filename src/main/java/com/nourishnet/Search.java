196847278d299005679b12ac29a59677d1189377
Thomas Elliott <70280680+obliviousmonkey1@users.noreply.github.com>
2024-02-05 15:01:44 +0000

    Searching

--- src/main/java/com/nourishnet/Search.java
+++ src/main/java/com/nourishnet/Search.java
@@ -1,11 +1,38 @@
 package com.nourishnet;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Search {
     
-    public static ArrayList<Recipe> getRecipeSearchResults(ArrayList<Recipe> recipeHolder, ArrayList<Integer> userSavedRecipes, int userSavedDiet){
-        // check through favourite recipes first 
-        // check through recipes with the same diet plan next 
+    public static ArrayList<Recipe> getRecipeSearchResults(String query, ArrayList<Recipe> recipeHolder, ArrayList<Integer> userSavedRecipes, int userSavedDiet) {
+        ArrayList<Recipe> returnedRecipes = new ArrayList<>();
+        Set<Recipe> uniqueRecipes = new HashSet<>();
+
+        for (int i = 0; i < recipeHolder.size(); i++) {
+            ArrayList<String> tags = recipeHolder.get(i).getTags();
+    
+            for (String tag : tags) {
+                System.out.println(tag); // debug
+                if (tag.toLowerCase().contains(query.toLowerCase())) {
+                    uniqueRecipes.add(recipeHolder.get(i));
+                    break;
+                }
+            }
+
+            if(recipeHolder.get(i).getName().toLowerCase().contains(query.toLowerCase())){
+                uniqueRecipes.add(recipeHolder.get(i));
+            }
+
+            if(recipeHolder.get(i).getLevel().toLowerCase().contains(query.toLowerCase())){
+                uniqueRecipes.add(recipeHolder.get(i));
+
+            }
+        }
+        returnedRecipes.addAll(uniqueRecipes);
+    
+        return returnedRecipes;
     }
+    
 }
