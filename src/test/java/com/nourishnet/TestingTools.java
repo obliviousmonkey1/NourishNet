4554f5fdeab4aa383764a3e7eed720f4bf747717
obliviousmonkey1 <70280680+obliviousmonkey1@users.noreply.github.com>
2024-02-26 14:35:01 +0000

    Updating Testing Tools

--- src/test/java/com/nourishnet/TestingTools.java
+++ src/test/java/com/nourishnet/TestingTools.java
@@ -1,5 +1,7 @@
 package com.nourishnet;
 
+import java.io.File;
+
 public class TestingTools {
 	
     // This method checks if the expected and actual values are equal; throws AssertionError if not
@@ -8,4 +10,19 @@ public class TestingTools {
             throw new AssertionError("Expected: " + expected + ", Actual: " + actual);
         }
     }
+
+    public static void assertFileExistence(String filePath, Boolean expectedToExist) {
+        File fileToCheck = new File(filePath);
+
+        if (expectedToExist) {
+            if (!fileToCheck.exists()) {
+                throw new AssertionError("File does not exist: " + filePath);
+            }
+        } else {
+            if (fileToCheck.exists()) {
+                throw new AssertionError("File exists: " + filePath);
+            }
+        }
+    }
+
 }
