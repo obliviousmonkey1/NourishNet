fb6c278111eb98b5b5cac5a7323913652ab45cc0
obliviousmonkey1 <70280680+obliviousmonkey1@users.noreply.github.com>
2024-02-26 14:26:18 +0000

    Changes testing

--- src/test/java/com/nourishnet/RecipeTest.java
+++ src/test/java/com/nourishnet/RecipeTest.java
@@ -1,8 +1,70 @@
 package com.nourishnet;
 
+import java.util.ArrayList;
+
 public class RecipeTest {
 
+    private static void testRecipeCreation() {
+        // Create a sample nutrition object
+        Recipe.Nutrition nutrition = new Recipe.Nutrition(200, 10, 5, 20, 10, 5, 15, 2);
+
+        // Create a sample ingredient list
+        ArrayList<Recipe.Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Recipe.Ingredient("Flour", 200, "grams"));
+        ingredients.add(new Recipe.Ingredient("Sugar", 50, "grams"));
+
+        // Create a sample recipe
+        Recipe recipe = new Recipe("Chocolate Cake", new ArrayList<String>(), 30, 60, "Easy", 8, nutrition, "Delicious chocolate cake recipe", ingredients, new ArrayList<String>(), new ArrayList<String>());
+
+        // Assert recipe attributes
+        TestingTools.AssertEquals("Chocolate Cake", recipe.getName());
+        TestingTools.AssertEquals(30, recipe.getPrepTime());
+        TestingTools.AssertEquals(60, recipe.getCookTime());
+        TestingTools.AssertEquals("Easy", recipe.getLevel());
+        TestingTools.AssertEquals(8, recipe.getServes());
+        TestingTools.AssertEquals(nutrition, recipe.getNutrition());
+        TestingTools.AssertEquals("Delicious chocolate cake recipe", recipe.getDescription());
+        TestingTools.AssertEquals(ingredients, recipe.getIngredients());
+    }
+
+    private static void testSettingAndGetters() {
+        // Create a sample recipe
+        Recipe recipe = new Recipe("", new ArrayList<String>(), 0, 0, "", 0, null, "", new ArrayList<Recipe.Ingredient>(), new ArrayList<String>(), new ArrayList<String>());
+
+        // Test setting and getting recipe name
+        recipe.setName("Test Recipe");
+        TestingTools.AssertEquals("Test Recipe", recipe.getName());
+
+        // Test setting and getting prep time
+        recipe.setPrepTime(20);
+        TestingTools.AssertEquals(20, recipe.getPrepTime());
+
+        // Test setting and getting cook time
+        recipe.setCookTime(40);
+        TestingTools.AssertEquals(40, recipe.getCookTime());
+
+        // Test setting and getting level
+        recipe.setLevel("Intermediate");
+        TestingTools.AssertEquals("Intermediate", recipe.getLevel());
+
+        // Test setting and getting serves
+        recipe.setServes(4);
+        TestingTools.AssertEquals(4, recipe.getServes());
+
+        // Test setting and getting description
+        recipe.setDescription("This is a test recipe description.");
+        TestingTools.AssertEquals("This is a test recipe description.", recipe.getDescription());
+
+        // Test setting and getting ingredients
+        ArrayList<Recipe.Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Recipe.Ingredient("Test Ingredient", 100, "grams"));
+        recipe.setIngredients(ingredients);
+        TestingTools.AssertEquals(ingredients, recipe.getIngredients());
+    }
+
     public static void Entry() {
-        System.out.println("RecipeTest.Entry() called");
-	}    
+        testRecipeCreation();
+        testSettingAndGetters();
+    }
 }
+
--- src/test/java/com/nourishnet/UserTest.java
+++ src/test/java/com/nourishnet/UserTest.java
@@ -1,10 +1,12 @@
 package com.nourishnet;
 
+import java.util.ArrayList;
+
 public class UserTest {
     /*  
      *  USER IDS 0000, 0001, 0002, 0003, 0004
      *  Get a user from the json file using        
-     *  User user = ResourceLoader.loadUser(LogIn.getUserJsonPath("ID HERE"));
+     *  User user = ResourceLoader.UserManager(LogIn.getUserJsonPath("ID HERE"));
      *  Use 0000 for testing saving a user to a json file
      *  Test creating a new user as well
      */
@@ -21,13 +23,55 @@ public class UserTest {
         SerializeJsonData.serializeNewUser(user);
     }
 
-    private static void testSaveUser(){
 
+ private static void testSaveUser() {
+        User user = new User();
+        System.out.println("Saving User ID: " + user.getId());
+        SerializeJsonData.serializeNewUser(user);
     }
     
+    private static void testSettingAndGetters() {
+        // Create a user
+        User user = new User();
+
+        // Test setting and getting username
+        user.setUsername("testUser");
+        TestingTools.AssertEquals("testUser", user.getUsername());
+
+        // Test setting and getting height
+        user.setHeight(180);
+        TestingTools.AssertEquals(180, user.getHeight());
+
+        // Test setting and getting weight
+        user.setWeight(75);
+        TestingTools.AssertEquals(75, user.getWeight());
+
+        // Test setting and getting diet
+        user.setDiet("Vegan");
+        TestingTools.AssertEquals("Vegan", user.getDiet());
+
+        // Test setting and getting savedRecipeIDs
+        ArrayList<Integer> savedRecipeIDs = new ArrayList<>();
+        savedRecipeIDs.add(1);
+        savedRecipeIDs.add(2);
+        user.setSavedRecipeIDs(savedRecipeIDs);
+        TestingTools.AssertEquals(savedRecipeIDs, user.getSavedRecipeIDs());
+    }
+
+    private static void testPasswordFunctionality() {
+        // Create a user with a password
+        User user = new User();
+        user.setPassword("password123");
+
+        // Test checking password
+        TestingTools.AssertEquals("password123", user.getPassword());
+    }
 
     public static void Entry() {
         testLoadUser();
-		testCreateAndSaveUser();
-	}
+        testCreateAndSaveUser();
+        testSaveUser();
+        testSettingAndGetters();
+        testPasswordFunctionality();
+    }
 }
