956c2da7b677e2142bc9ff1e25676b38442b9efc
decroe <157384166+decroe@users.noreply.github.com>
2024-04-16 14:53:53 +0100

    Update DietTest.java (DeclanV2) FINAL

--- src/test/java/com/nourishnet/DietTest.java
+++ src/test/java/com/nourishnet/DietTest.java
@@ -12,13 +12,14 @@ package com.nourishnet;
 import java.util.ArrayList;
 
 public class DietTest {
+
     
     private static ArrayList<Diet> diets;
 
     private static void testDietLoading() {
         
-         // Loading in the diets from the json file
-        
+        //Loading in the diets from the json file while also showing use of encapsulation in 
+        // being only accessed in the DietTest class which is helping data integritiy 
         diets = ResourceLoader.loadDiets();
 
         // Checking to see if the number of diet loaded is equal to the number of diet in the diet folder
@@ -27,26 +28,23 @@ public class DietTest {
     
 
 
-     private static void testGetExampleDiets((ArrayList<Diet> sampleDiets) {
-        System.out.println("Testing getExampleDiets method...");
-        //Diet objects loaded for testing
-        ArrayList<Diet> sampleDiets = ResourceLoader.loadDietsFromJson("Diets.json");
-
-         
-        //Need populating sampleDiets with some test data
+    private static void testGetExampleDiets(ArrayList<Diet> sampleDiets) {
         
-        //Looping through each diet and testing the method
+        System.out.println("Testing getExampleDiets method...");
+       
+        //Looping through each diet and testing the method along with giving the diet recipe examples
         for (Diet diet : diets) {
-            ArrayList<Diets> exampleDiets = diet.getExampleDiets(sampleDiets);
+            ArrayList<Recipe> exampleRecipes = diet.getExampleRecipes(sampleDiets);
             System.out.println("Diet: " + diet.getName());
-            System.out.println("Example Diets: ");
-            for (Diet diet : exampleDiets) {
-                System.out.println("- " + recipe.getName());
-            }
-            System.out.println("---------------");
+            System.out.println("Example Recipes: ");
+            
+            for (Recipe recipe : exampleRecipes) {
+            System.out.println("- " + recipe.getName());
         }
-        System.out.println("getExampleDiets method tested successfully.");
+        System.out.println("---------------");
     }
+    System.out.println("getExampleDiets method tested successfully.");
+}
 
 
 
@@ -62,6 +60,13 @@ public class DietTest {
     }
     
       public static void Entry() {
+        
+        // Loading diets from JSON file
+        diets = ResourceLoader.loadDiets();
+        
+        // Loading sample diets from JSON file
+        ArrayList<Diet> sampleDiets = ResourceLoader.loadDietsFromJson("Diets.json");
+
         testDietLoading();
         testGetExampleDiets(sampleDiets);
         testGetName();
